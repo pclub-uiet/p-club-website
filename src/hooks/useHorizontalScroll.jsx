30434bbcc0a4c44b68bd731519d0267e545ebb9e
krishna jain <krishnajain5050@gmail.com>
2025-02-01 17:35:55 +0530

    added team section with horizontal scroll

---
+++ src/hooks/useHorizontalScroll.jsx
@@ -0,0 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+export const useHorizontalScroll = (containerRef, sections) => {
+  useGSAP(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: containerRef.current,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: () => "+=" + containerRef.current.offsetWidth,
+      },
+    });
+  });
+};
